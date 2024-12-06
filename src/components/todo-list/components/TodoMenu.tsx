99667c5b172e51606bf368b2641dba347724c004
LeeByeongMuk <mansogod@gmail.com>
2024-12-07 00:24:58 +0900

    feat: add TodoMenu component for filtering todos

---
+++ src/components/todo-list/components/TodoMenu.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import styled from "@emotion/styled";
+import {Filter} from "../types/types";
+
+interface TodoMenuProps {
+  options: Filter[];
+  currentFilter: Filter;
+  onFilterChange: (filter: Filter) => void;
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Button = styled.button<{ active: boolean }>`
+  width: 108px;
+  height: 40px;
+  border-radius: 12px;
+  background-color: ${({active}) => (active ? "#EBF4FF" : "#FFFFFF")};
+  color: ${({active}) => (active ? "#2182F3" : "#454545")};
+  text-align: center;
+  font-family: "Pretendard Variable", Pretendard, -apple-system,
+  BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI",
+  "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji",
+  "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 40px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #ebf4ff;
+    color: #2182f3;
+  }
+`;
+
+export default function TodoMenu({options, currentFilter, onFilterChange}: TodoMenuProps) {
+  return (
+    <Container>
+      {options.map((option) => (
+        <Button
+          key={option}
+          active={currentFilter === option}
+          onClick={() => onFilterChange(option)}
+        >
+          {option.toUpperCase()}
+        </Button>
+      ))}
+    </Container>
+  );
+};
