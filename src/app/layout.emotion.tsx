08a1228bd65c225d3b7bc08cae79ae356254e0e3
LeeByeongMuk <mansogod@gmail.com>
2024-12-07 21:50:22 +0900

    style: Emotion 테마 설정 및 Pretendard 폰트 추가 (#2)
    * style: integrate Emotion for styling and theme management
    
    * Fix styling
    
    * chore: update GitHub Actions workflows
    
    * chore: update GitHub Actions workflows
    
    * style: add Pretendard font to project
    
    ---------
    
    Co-authored-by: LeeByeongMuk <LeeByeongMuk@users.noreply.github.com>

---
+++ src/app/layout.emotion.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { ThemeProvider } from '@emotion/react';
+import React from 'react';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const theme = {
+  colors: {
+    background: '#F6F6F6',
+    primary100: '#EBF4FF',
+    primary200: '#2182F3',
+    textPrimary: '#000000',
+    textSecondary: '#333333',
+    textTertiary: '#454545',
+    textMuted: '#868686',
+    textDisabled: '#B9B9B9',
+    border: '#E5E5E5',
+  },
+  devices: {
+    medium: '737px',
+  },
+};
+
+const LayoutRecoil = ({ children }: Props) => {
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+};
+
+export default LayoutRecoil;
