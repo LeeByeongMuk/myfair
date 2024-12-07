800db9ce79fd76f189181bc89a330866f2dac25d
LeeByeongMuk <mansogod@gmail.com>
2024-12-07 18:40:15 +0900

    style: fix TodoMenu lint and formatting issues with ESLint and Prettier

--- src/components/todo-list/components/TodoMenu.tsx
+++ src/components/todo-list/components/TodoMenu.tsx
@@ -1,11 +1,12 @@
-import React from "react";
-import styled from "@emotion/styled";
-import {Filter} from "../types/types";
+import styled from '@emotion/styled';
+import React from 'react';
+
+import { Filter } from '../types/types';
 
 interface TodoMenuProps {
   options: Filter[];
   currentFilter: Filter;
-  onFilterChange: (filter: Filter) => void;
+  handleChangeFilter: (filter: Filter) => void;
 }
 
 const Container = styled.div`
@@ -17,13 +18,25 @@ const Button = styled.button<{ active: boolean }>`
   width: 108px;
   height: 40px;
   border-radius: 12px;
-  background-color: ${({active}) => (active ? "#EBF4FF" : "#FFFFFF")};
-  color: ${({active}) => (active ? "#2182F3" : "#454545")};
+  background-color: ${({ active }) => (active ? '#EBF4FF' : '#FFFFFF')};
+  color: ${({ active }) => (active ? '#2182F3' : '#454545')};
   text-align: center;
-  font-family: "Pretendard Variable", Pretendard, -apple-system,
-  BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI",
-  "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji",
-  "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
+  font-family:
+    'Pretendard Variable',
+    Pretendard,
+    -apple-system,
+    BlinkMacSystemFont,
+    system-ui,
+    Roboto,
+    'Helvetica Neue',
+    'Segoe UI',
+    'Apple SD Gothic Neo',
+    'Noto Sans KR',
+    'Malgun Gothic',
+    'Apple Color Emoji',
+    'Segoe UI Emoji',
+    'Segoe UI Symbol',
+    sans-serif;
   font-size: 16px;
   font-weight: 600;
   line-height: 40px;
@@ -35,18 +48,22 @@ const Button = styled.button<{ active: boolean }>`
   }
 `;
 
-export default function TodoMenu({options, currentFilter, onFilterChange}: TodoMenuProps) {
+export default function TodoMenu({
+  options,
+  currentFilter,
+  handleChangeFilter,
+}: TodoMenuProps) {
   return (
     <Container>
-      {options.map((option) => (
+      {options.map(option => (
         <Button
           key={option}
           active={currentFilter === option}
-          onClick={() => onFilterChange(option)}
+          onClick={() => handleChangeFilter(option)}
         >
           {option.toUpperCase()}
         </Button>
       ))}
     </Container>
   );
-};
+}
