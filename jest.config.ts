d850982255dce069aa6fb6b387c8735a8789f54d
LeeByeongMuk <mansogod@gmail.com>
2024-12-08 23:16:00 +0900

    chore: configure Jest with ts-jest for TypeScript testing

--- jest.config.ts
+++ jest.config.ts
@@ -1,15 +1,21 @@
-import type { Config } from "jest";
+import type { Config } from 'jest';
+import nextJest from 'next/jest';
+import { pathsToModuleNameMapper } from 'ts-jest';
 
-const nextJest = require("next/jest");
+import { compilerOptions } from './tsconfig.json';
 
 const createJestConfig = nextJest({
   // next.config.js 및 .env 파일을 로드할 Next.js 앱의 경로
-  dir: "./",
+  dir: './',
 });
 
 const customJestConfig: Config = {
-  testEnvironment: "jest-environment-jsdom",
+  testEnvironment: 'jest-environment-jsdom',
   verbose: true,
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/', // 루트 디렉토리 기준 경로 설정
+  }),
+  collectCoverage: false
 };
 
 module.exports = createJestConfig(customJestConfig);
