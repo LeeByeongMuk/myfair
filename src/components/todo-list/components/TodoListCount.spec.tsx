5bb2e737aee72a786374d6602fe7bd8b571949cc
LeeByeongMuk <mansogod@gmail.com>
2024-12-10 15:44:58 +0900

    test: add unit tests for TodoListCount component

---
+++ src/components/todo-list/components/TodoListCount.spec.tsx
@@ -0,0 +1,20 @@
+import '@testing-library/jest-dom';
+
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+
+import TodoListCount from '@components/todo-list/components/TodoListCount';
+
+describe('TodoListCount 컴포넌트', () => {
+  test('totalCount 값이 0일 때 올바르게 렌더링된다.', () => {
+    render(<TodoListCount totalCount={0} />);
+
+    expect(screen.getByText(/총 0개/i)).toBeInTheDocument();
+  });
+
+  test('totalCount 값이 10일 때 올바르게 렌더링된다.', () => {
+    render(<TodoListCount totalCount={10} />);
+
+    expect(screen.getByText(/총 10개/i)).toBeInTheDocument();
+  });
+});
